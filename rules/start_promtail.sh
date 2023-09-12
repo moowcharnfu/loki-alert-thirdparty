08b49b7ef46ac8863c9f662993eede5fa2f4c34b
moowcharnfu <fmc2012cmf@163.com>
2023-09-12 15:52:52 +0800

    Delete rules/start_promtail.sh

--- rules/start_promtail.sh
+++
@@ -1,9 +0,0 @@
-# kill port
-ps -ef | grep promtail-linux-amd64 | awk '{print $2}' | xargs kill -9
-# file positions 
-echo "" > /tmp/positions.yaml
-echo "1 0 * * * echo '' > /tmp/positions.yaml" > /etc/crontabs/root
-# chmod excutable
-chmod a+rwx /tmp/positions.yaml /opt/run/promtail-linux-amd64 /opt/run/start_promtail.sh
-# start
-nohup /opt/run/promtail-linux-amd64 -config.file=/opt/run/promtail.yaml -print-config-stderr=true >/tmp/promtail.log 2>&1 &
